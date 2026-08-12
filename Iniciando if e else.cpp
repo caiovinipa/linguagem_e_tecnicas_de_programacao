a44e5af17ca67c7f42ca5a7f169b053d463a71db
Caio Vinicius <caio.vinicius.patrono@gmail.com>
2026-08-12 20:32:54 -0300

    Add files via upload

---
+++ Iniciando if e else.cpp
@@ -0,0 +1,12 @@
+#inlcude <stdio.h>
+#include <locale.h>
+//== Sinal de "e" é igual a ==, sinal de "ou" é igual a ||.
+main(){
+	setlocale(LC_ALL, "Portuguese");
+	a = 10;
+	b = 20;
+	if(a == 10){
+	printf("A é igual a 10 ");	
+	}
+//== else significa: isso senão isso
+}
