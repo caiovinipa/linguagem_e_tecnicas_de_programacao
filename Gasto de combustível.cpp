d7d89fe856e0f2860d0d6dfd92c974f6058a8c08
Caio Vinicius <caio.vinicius.patrono@gmail.com>
2026-08-19 19:16:32 -0300

    Add files via upload

--- Gasto de combustível.cpp
+++ Gasto de combustível.cpp
@@ -3,13 +3,13 @@
 main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	char tipo;
+	int tipo;
 	float capacidade, valor, gasolina = 6.50, alcool = 4.50;
 		printf("Digite a capacidade máxima do carro em litros: ");
 		scanf("%f", &capacidade);
-		printf("Ele utiliza qual tipo de combustível?\nDigite G para gasolina e A para álcool: ");
-		scanf("%c", &tipo);
-	if (tipo == "G" ){
+		printf("Ele utiliza qual tipo de combustível?\nDigite 1 para gasolina e 2 para álcool: ");
+		scanf("%d", &tipo);
+	if (tipo == 1 ){
 	valor = gasolina * capacidade; 
 		printf("Com %.2fL, gastaria R$%.2f", capacidade, valor);
 	}else{
